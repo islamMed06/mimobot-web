3f6fd5f09782d978dd70f2c693f20613eea4edfc
MimoBot <mimobot@bot.local>
2026-06-16 16:17:32 +0100

    Fix chemins CSS react-pdf

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,8 +2,8 @@
 
 import { useState, useRef, useCallback, useEffect } from "react";
 import { pdfjs, Document, Page } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
 
